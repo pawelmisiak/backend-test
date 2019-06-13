6c2d6554c1e1b6edc8d1ef428644cf4fd51dbfa5
pawelmisiak <thepawelmisiak@gmail.com>
2019-06-13 10:44:00 -0400

    fix

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 
 module.exports = new Sequelize("postgres", "postgres", "123", {
-  host: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
   // host: "postgresql-silhouetted-48671",
   ssl: true,
   dialect: "postgres"
