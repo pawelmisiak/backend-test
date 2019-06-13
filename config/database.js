3ba1b372399c1f0e2202a4f896b6b97caf0ca38c
pawelmisiak <thepawelmisiak@gmail.com>
2019-06-13 10:42:08 -0400

    adding database connection

--- config/database.js
+++ config/database.js
@@ -1,6 +1,8 @@
 const Sequelize = require("sequelize");
 
 module.exports = new Sequelize("postgres", "postgres", "123", {
-  host: "postgresql-silhouetted-48671",
+  host: process.env.DATABASE_URL,
+  // host: "postgresql-silhouetted-48671",
+  ssl: true,
   dialect: "postgres"
 });
