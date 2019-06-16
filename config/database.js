4a27ace2f6c071f819bb50f1ff380ac864237107
pawelmisiak <thepawelmisiak@gmail.com>
2019-06-16 16:36:33 -0400

    commented out local database

--- config/database.js
+++ config/database.js
@@ -1,9 +1,14 @@
 const Sequelize = require("sequelize");
 
-module.exports = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
-  protocol: "postgres",
-  dialectOptions: {
-    ssl: true
-  }
+// module.exports = new Sequelize(process.env.DATABASE_URL, {
+//   dialect: "postgres",
+//   protocol: "postgres",
+//   dialectOptions: {
+//     ssl: true
+//   }
+// });
+
+module.exports = new Sequelize("postgres", "postgres", "123", {
+  host: "localhost",
+  dialect: "postgres"
 });
